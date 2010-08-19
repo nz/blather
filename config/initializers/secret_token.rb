b48689a7b4ecc8a002cb560838034301a62001ca
Nick Zadrozny <nick@beyondthepath.com>
2010-08-19 11:48:00 -0700

    Starting on a demo app for Rails 3 and Sunspot with Websolr

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blather::Application.config.secret_token = '33b916df6a6998562c7d8278cb191f6fe5c88e26025d9112aa9be8c22a3d601b5adba61f0efdd18da3df0ba732539ffbc9db106b23c2d18a3ab283cbcdf80009'
